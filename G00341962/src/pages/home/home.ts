eea231ec3802fa6a00d2396eacb688b091c134b5
Bernard Wong <ben.wc88@gmail.com>
2018-05-03 03:19:58 +0100

    Edit Home Page
    Edit home page to navigate through each pages.

--- G00341962/src/pages/home/home.ts
+++ G00341962/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PresetPage } from '../preset/preset';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,18 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+  }
 
+  toMorseCodes(){
+    this.navCtrl.push("MorseCodesPage");
+  }
+
+  toPreset(){
+    this.navCtrl.push(PresetPage);
+  }
+
+  toEncryption(){
+    this.navCtrl.push("EncryptionPage");
   }
 
 }
